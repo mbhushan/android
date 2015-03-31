f4665c14c29f89b5f0b6f2232165c4bba1d49875
manibhushan <manibhushan.cs@gmail.com>
2015-03-31 07:09:26 +0530

    fulltest suite

---
+++ Sunshine/app/src/androidTest/java/com/mani/sunshine/FullTestSuite.java
@@ -0,0 +1,20 @@
+package com.mani.sunshine;
+
+/**
+ * Created by manib on 31/03/15.
+ */
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
