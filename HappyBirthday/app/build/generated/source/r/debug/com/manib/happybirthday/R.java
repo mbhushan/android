e0d7693b3f3dc385a48b0ef26cae4e7b311f9b67
manibhushan <manibhushan.cs@gmail.com>
2015-09-15 21:54:56 +0530

    happy birthday app

--- HappyBirthday/app/build/generated/source/r/debug/com/manib/happybirthday/R.java
+++ HappyBirthday/app/build/generated/source/r/debug/com/manib/happybirthday/R.java
@@ -1325,7 +1325,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09003f;
+        public static final int action_settings=0x7f090040;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090016;
         public static final int beginning=0x7f090013;
@@ -1371,6 +1371,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090007;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000b;
+        public static final int textView=0x7f09003f;
         public static final int title=0x7f090027;
         public static final int up=0x7f090008;
         public static final int useLogo=0x7f090012;
@@ -1433,7 +1434,8 @@ containing a value of this type.
         public static final int abc_toolbar_collapse_description=0x7f0b000f;
         public static final int action_settings=0x7f0b0010;
         public static final int app_name=0x7f0b0011;
-        public static final int hello_world=0x7f0b0012;
+        public static final int happy_bday=0x7f0b0012;
+        public static final int hello_world=0x7f0b0013;
     }
     public static final class style {
         public static final int Animation_AppCompat_DropDownUp=0x7f0c0000;
