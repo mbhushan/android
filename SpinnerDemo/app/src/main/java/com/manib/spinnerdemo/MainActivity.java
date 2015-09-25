af8b36934a8a39aa5afbff55d1f318009c79ff17
manibhushan <manibhushan.cs@gmail.com>
2015-09-25 10:44:07 +0530

    added onClick event for spinner

--- SpinnerDemo/app/src/main/java/com/manib/spinnerdemo/MainActivity.java
+++ SpinnerDemo/app/src/main/java/com/manib/spinnerdemo/MainActivity.java
@@ -4,14 +4,57 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private String[] celebrities = {
+            "Chris Hemsworth",
+            "Jennifer Lawrence",
+            "Jessica Alba",
+            "Brad Pitt",
+            "Tom Cruise",
+            "Johnny Depp",
+            "Megan Fox",
+            "Paul Walker",
+            "Vin Diesel",
+            "Kevin Spacey",
+            "Kevin Bacon",
+            "Mani Bhushan"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_items);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, celebrities);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener () {
+                    public void onItemSelected( AdapterView<?> parent, View view, int position, long id) {
+                        String selection = parent.getItemAtPosition(position).toString();
+                        Toast toast = Toast.makeText(getApplicationContext(), selection, Toast.LENGTH_SHORT);
+                        toast.show();
+
+                    }
+
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Nothing Selected!", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+        );
+
     }
 
 
