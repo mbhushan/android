ac4c573342d3f3f49e686010b2b0edb7bed5bd4a
manibhushan <manibhushan.cs@gmail.com>
2015-09-27 17:39:50 +0530

    experimenting with list view

---
+++ ListView/app/src/main/java/com/manib/listview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.manib.listview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+public class MainActivity extends ActionBarActivity implements OnItemClickListener {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.sportslist);
+        listView.setOnItemClickListener(this);
+    }
+
+    /*
+     * Parameters:
+        adapter - The AdapterView where the click happened.
+        view - The view within the AdapterView that was clicked
+        position - The position of the view in the adapter.
+        id - The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
