64fab0fe4f85214f34fe4005f1b0d238634864b2
manibhushan <manibhushan.cs@gmail.com>
2015-09-27 18:43:30 +0530

    array adapter implementation with detail activity

--- ArrayAdapter/app/build/generated/source/r/debug/com/manib/arrayadapter/R.java
+++ ArrayAdapter/app/build/generated/source/r/debug/com/manib/arrayadapter/R.java
@@ -1526,7 +1526,7 @@ containing a value of this type.
         public static final int notification_template_icon_bg=0x7f02003b;
     }
     public static final class id {
-        public static final int action0=0x7f09004f;
+        public static final int action0=0x7f090051;
         public static final int action_bar=0x7f090040;
         public static final int action_bar_activity_content=0x7f090000;
         public static final int action_bar_container=0x7f09003f;
@@ -1535,21 +1535,21 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f090024;
         public static final int action_bar_title=0x7f090023;
         public static final int action_context_bar=0x7f090041;
-        public static final int action_divider=0x7f090053;
+        public static final int action_divider=0x7f090055;
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_bar=0x7f09003d;
         public static final int action_mode_bar_stub=0x7f09003c;
         public static final int action_mode_close_button=0x7f090025;
-        public static final int action_settings=0x7f09005c;
+        public static final int action_settings=0x7f09005e;
         public static final int activity_chooser_view_content=0x7f090026;
         public static final int alertTitle=0x7f090030;
         public static final int always=0x7f090016;
         public static final int beginning=0x7f090013;
         public static final int buttonPanel=0x7f090036;
-        public static final int cancel_action=0x7f090050;
+        public static final int cancel_action=0x7f090052;
         public static final int checkbox=0x7f090038;
-        public static final int chronometer=0x7f090056;
+        public static final int chronometer=0x7f090058;
         public static final int collapseActionView=0x7f090017;
         public static final int contentPanel=0x7f090031;
         public static final int custom=0x7f090035;
@@ -1561,7 +1561,7 @@ containing a value of this type.
         public static final int dropdown=0x7f09001c;
         public static final int edit_query=0x7f090042;
         public static final int end=0x7f090014;
-        public static final int end_padder=0x7f09005b;
+        public static final int end_padder=0x7f09005d;
         public static final int expand_activities_button=0x7f090027;
         public static final int expanded_menu=0x7f090037;
         public static final int home=0x7f090004;
@@ -1569,12 +1569,13 @@ containing a value of this type.
         public static final int icon=0x7f09002b;
         public static final int ifRoom=0x7f090018;
         public static final int image=0x7f090028;
-        public static final int info=0x7f09005a;
-        public static final int line1=0x7f090054;
-        public static final int line3=0x7f090058;
+        public static final int info=0x7f09005c;
+        public static final int line1=0x7f090056;
+        public static final int line3=0x7f09005a;
         public static final int listMode=0x7f090009;
         public static final int list_item=0x7f09002a;
-        public static final int media_actions=0x7f090052;
+        public static final int listview=0x7f090050;
+        public static final int media_actions=0x7f090054;
         public static final int middle=0x7f090015;
         public static final int multiply=0x7f09001e;
         public static final int never=0x7f090019;
@@ -1605,13 +1606,14 @@ containing a value of this type.
         public static final int src_atop=0x7f090020;
         public static final int src_in=0x7f090021;
         public static final int src_over=0x7f090022;
-        public static final int status_bar_latest_event_content=0x7f090051;
+        public static final int status_bar_latest_event_content=0x7f090053;
         public static final int submit_area=0x7f09004b;
         public static final int tabMode=0x7f09000b;
-        public static final int text=0x7f090059;
-        public static final int text2=0x7f090057;
+        public static final int text=0x7f09005b;
+        public static final int text2=0x7f090059;
         public static final int textSpacerNoButtons=0x7f090033;
-        public static final int time=0x7f090055;
+        public static final int textview=0x7f09004f;
+        public static final int time=0x7f090057;
         public static final int title=0x7f09002c;
         public static final int title_template=0x7f09002f;
         public static final int topPanel=0x7f09002e;
@@ -1653,22 +1655,25 @@ containing a value of this type.
         public static final int abc_search_view=0x7f040016;
         public static final int abc_select_dialog_material=0x7f040017;
         public static final int abc_simple_dropdown_hint=0x7f040018;
-        public static final int activity_main=0x7f040019;
-        public static final int notification_media_action=0x7f04001a;
-        public static final int notification_media_cancel_action=0x7f04001b;
-        public static final int notification_template_big_media=0x7f04001c;
-        public static final int notification_template_big_media_narrow=0x7f04001d;
-        public static final int notification_template_lines=0x7f04001e;
-        public static final int notification_template_media=0x7f04001f;
-        public static final int notification_template_part_chronometer=0x7f040020;
-        public static final int notification_template_part_time=0x7f040021;
-        public static final int select_dialog_item_material=0x7f040022;
-        public static final int select_dialog_multichoice_material=0x7f040023;
-        public static final int select_dialog_singlechoice_material=0x7f040024;
-        public static final int support_simple_spinner_dropdown_item=0x7f040025;
+        public static final int activity_detail=0x7f040019;
+        public static final int activity_main=0x7f04001a;
+        public static final int list_item=0x7f04001b;
+        public static final int notification_media_action=0x7f04001c;
+        public static final int notification_media_cancel_action=0x7f04001d;
+        public static final int notification_template_big_media=0x7f04001e;
+        public static final int notification_template_big_media_narrow=0x7f04001f;
+        public static final int notification_template_lines=0x7f040020;
+        public static final int notification_template_media=0x7f040021;
+        public static final int notification_template_part_chronometer=0x7f040022;
+        public static final int notification_template_part_time=0x7f040023;
+        public static final int select_dialog_item_material=0x7f040024;
+        public static final int select_dialog_multichoice_material=0x7f040025;
+        public static final int select_dialog_singlechoice_material=0x7f040026;
+        public static final int support_simple_spinner_dropdown_item=0x7f040027;
     }
     public static final class menu {
-        public static final int menu_main=0x7f0d0000;
+        public static final int menu_detail=0x7f0d0000;
+        public static final int menu_main=0x7f0d0001;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
@@ -1695,6 +1700,7 @@ containing a value of this type.
         public static final int app_name=0x7f0b0012;
         public static final int hello_world=0x7f0b0013;
         public static final int status_bar_notification_info_overflow=0x7f0b0014;
+        public static final int title_activity_detail=0x7f0b0015;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat=0x7f0c0000;
--- ArrayAdapter/app/src/main/java/com/manib/arrayadapter/MainActivity.java
+++ ArrayAdapter/app/src/main/java/com/manib/arrayadapter/MainActivity.java
@@ -4,16 +4,49 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String EXTRA_TEXT = "sample";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] myArray = {"A.M.", "Being There", "Summerteeth", "Yankee Hotel Foxtrot",
+                "A Ghost Is Born", "Kicking Television: Live in Chicago", "Sky Blue Sky",
+                "Wilco (The Album)", "The Whole Love", "Star Wars"};
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, R.layout.list_item, myArray);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView clickedTextView = (TextView) view.findViewById(R.id.textview);
+                String text = clickedTextView.getText().toString();
+                startDetailActivity(text);
+
+            }
+        });
     }
 
+    private void startDetailActivity(String text) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(EXTRA_TEXT, text);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
