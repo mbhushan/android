4b31a6ce09047f37800219e9a53a9bcc17745268
manibhushan <manibhushan.cs@gmail.com>
2015-09-27 10:31:03 +0530

    added tab2 code and config

---
+++ SwapApp/app/src/main/java/com/manib/swapapp/Tab2.java
@@ -0,0 +1,28 @@
+package com.manib.swapapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by manib on 27/09/15.
+ */
+public class Tab2 extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static Tab2 newInstance(int sectionNumber) {
+        Tab2 fragment = new Tab2();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab2,container,false);
+        return v;
+    }
+}
