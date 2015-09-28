de89fb96819c6fe76db388473beda841ed4eed78
manibhushan <manibhushan.cs@gmail.com>
2015-09-28 11:01:38 +0530

    ImageAdapter implementation

--- HelloGridView/app/src/main/java/com/manib/hellogridview/MainActivity.java
+++ HelloGridView/app/src/main/java/com/manib/hellogridview/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
     }
 
 
