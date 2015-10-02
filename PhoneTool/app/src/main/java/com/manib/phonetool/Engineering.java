37cfac034f06b4798aae96087b93af46c29ac7db
manibhushan <manibhushan.cs@gmail.com>
2015-10-02 08:45:30 +0530

    swipe and contact implementation

--- PhoneTool/app/src/main/java/com/manib/phonetool/Engineering.java
+++ PhoneTool/app/src/main/java/com/manib/phonetool/Engineering.java
@@ -1,18 +1,63 @@
 package com.manib.phonetool;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class Engineering extends ActionBarActivity {
+
+    private int[] mImages = new int[] {
+            R.drawable.anand,
+            R.drawable.ks,
+            R.drawable.atulm,
+            R.drawable.manib,
+            R.drawable.sidg,
+            R.drawable.suresh,
+            R.drawable.mukund,
+            R.drawable.murthy,
+            R.drawable.vaishakh,
+            R.drawable.vinitb,
+    };
+
+    private String [] names = {
+            "Anand Jain",
+            "Kumar Siddhartha",
+            "Atul Manwar",
+            "Mani Bhushan",
+            "Siddardha Garimella",
+            "Suresh Mopuru",
+            "Mukund K",
+            "Murthy Kaja",
+            "Vaishakh NR",
+            "Vinit Bothra",
+    };
+
+    private String[] mMobileNums = new String[]{
+            "tel:9845270455",
+            "tel:7259102000",
+            "tel:7760336453",
+            "tel:9739099370",
+            "tel:9008846426",
+            "tel:8892150510",
+            "tel:9739314574",
+            "tel:9611338758",
+            "tel:7411993776",
+            "tel:9591236124"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +65,7 @@ public class Engineering extends ActionBarActivity {
         setContentView(R.layout.activity_engineering);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
-        ImagePagerAdapter adapter = new ImagePagerAdapter();
+        ImagePagerAdapter adapter = new ImagePagerAdapter(Engineering.this, mImages, names, mMobileNums);
         viewPager.setAdapter(adapter);
     }
 
@@ -35,24 +80,22 @@ public class Engineering extends ActionBarActivity {
     }
 
     private class ImagePagerAdapter extends PagerAdapter {
-        private int[] mImages = new int[] {
-                R.drawable.anand,
-                R.drawable.venky,
-                R.drawable.saurav,
-                R.drawable.ks,
-                R.drawable.atulm,
-                R.drawable.manib,
-                R.drawable.sidg,
-                R.drawable.suresh,
-                R.drawable.mukund,
-                R.drawable.murthy,
-                R.drawable.vaishakh,
-                R.drawable.vinitb,
-                R.drawable.anupam,
-                R.drawable.pushpendra,
-                R.drawable.rajeshv,
-                R.drawable.rakesh
-        };
+
+        private Button click;
+        private Context context;
+        private int[] mImages;
+        private String[] names;
+        private String[] mMobileNums;
+
+
+        public ImagePagerAdapter(Context context, int[] mImages, String[] names, String[] mMobileNums) {
+            this.context = context;
+            this.mImages = mImages;
+            this.names = names;
+            this.mMobileNums = mMobileNums;
+
+        }
+
 
         @Override
         public int getCount() {
@@ -61,26 +104,52 @@ public class Engineering extends ActionBarActivity {
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return view == ((ImageView) object);
+            return view == ((RelativeLayout) object);
         }
 
         @Override
-        public Object instantiateItem(ViewGroup container, int position) {
+        public Object instantiateItem(ViewGroup container, final int position) {
             Context context = Engineering.this;
-            ImageView imageView = new ImageView(context);
+
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View convertView = inflater.inflate(R.layout.hres, container, false);
+            ImageView imageView = (ImageView) convertView
+                    .findViewById(R.id.imageHR);
+
             int padding = context.getResources().getDimensionPixelSize(
                     R.dimen.padding_medium);
             imageView.setPadding(padding, padding, padding, padding);
             imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
             imageView.setImageResource(mImages[position]);
 
-            ((ViewPager) container).addView(imageView, 0);
-            return imageView;
+            TextView topText = (TextView) convertView
+                    .findViewById(R.id.topText);
+            topText.setText(names[position]);
+
+
+
+            Button click = (Button) convertView.findViewById(R.id.btn_contact);
+            click.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse(mMobileNums[position]));
+                    startActivity(callIntent);
+
+                }
+            });
+            ((ViewPager) container).addView(convertView, 0);
+            return convertView;
+
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            ((ViewPager) container).removeView((ImageView) object);
+            ((ViewPager) container).removeView((RelativeLayout) object);
         }
     }
 
--- PhoneTool/app/src/main/java/com/manib/phonetool/HR.java
+++ PhoneTool/app/src/main/java/com/manib/phonetool/HR.java
@@ -25,7 +25,7 @@ import org.w3c.dom.Text;
 public class HR extends ActionBarActivity {
 
     public static ViewPager viewPager;
-    private static String [] names = {
+    private static String[] names = {
 
             "Anuradha Bansal",
             "Ajith P",
@@ -35,6 +35,22 @@ public class HR extends ActionBarActivity {
 
     };
 
+    private int[] mImages = new int[]{
+            R.drawable.anu,
+            R.drawable.ajith,
+            R.drawable.sneha,
+            R.drawable.prajwal,
+            R.drawable.rinki,
+    };
+
+    private String[] mMobileNums = new String[]{
+            "tel:9632251047",
+            "tel:9844145874",
+            "tel:9964972031",
+            "tel:9481510427",
+            "tel:9739099370",
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,29 +58,26 @@ public class HR extends ActionBarActivity {
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
 
-        ImagePagerAdapter adapter = new ImagePagerAdapter();
+        ImagePagerAdapter adapter = new ImagePagerAdapter(HR.this, mImages, names, mMobileNums);
         viewPager.setAdapter(adapter);
     }
 
     private class ImagePagerAdapter extends PagerAdapter {
 
         private Button click;
+        private Context context;
+        private int[] mImages;
+        private String[] names;
+        private String[] mMobileNums;
 
-        private int[] mImages = new int[] {
-                R.drawable.anu,
-                R.drawable.ajith,
-                R.drawable.sneha,
-                R.drawable.prajwal,
-                R.drawable.rinki,
-        };
 
-        private String[] mMobileNums = new String[] {
-                "tel:9632251047",
-                "tel:9844145874",
-                "tel:9964972031",
-                "tel:9481510427",
-                "tel:9481510427",
-        };
+        public ImagePagerAdapter(Context context, int[] mImages, String[] names, String[] mMobileNums) {
+            this.context = context;
+            this.mImages = mImages;
+            this.names = names;
+            this.mMobileNums = mMobileNums;
+
+        }
 
 
         @Override
@@ -81,51 +94,27 @@ public class HR extends ActionBarActivity {
         public Object instantiateItem(ViewGroup container, final int position) {
 
             Context context = HR.this;
-            /*
-            int currentItem= HR.viewPager.getCurrentItem();
 
-            ImageView imageView = new ImageView(context);
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View convertView = inflater.inflate(R.layout.hres, container, false);
+            ImageView imageView = (ImageView) convertView
+                    .findViewById(R.id.imageHR);
+
             int padding = context.getResources().getDimensionPixelSize(
                     R.dimen.padding_medium);
             imageView.setPadding(padding, padding, padding, padding);
             imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
             imageView.setImageResource(mImages[position]);
 
-            if(currentItem==position) {
-                TextView topTextItem = (TextView) findViewById(R.id.topText);
-                topTextItem.setText(HR.names[position]);
+            TextView topText = (TextView) convertView
+                    .findViewById(R.id.topText);
+            topText.setText(HR.names[position]);
 
 
-                click = (Button) findViewById(R.id.btn_contact);
-                click.setOnClickListener(new View.OnClickListener() {
 
-                    @Override
-                    public void onClick(View v) {
-                        // TODO Auto-generated method stub
-                        Intent callIntent = new Intent(Intent.ACTION_CALL);
-                        callIntent.setData(Uri.parse(mMobileNums[position]));
-                        startActivity(callIntent);
-
-                    }
-                });
-            }
-            Log.v("Position: ", String.valueOf(position));
-
-            ((ViewPager) container).addView(imageView, 0);
-
-            */
-                LayoutInflater inflater = (LayoutInflater) context
-                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-               View  convertView = inflater.inflate(R.layout.hres,null);
-                ImageView image = (ImageView) convertView
-                        .findViewById(R.id.image);
-                TextView topText = (TextView) convertView
-                        .findViewById(R.id.topText);
-                topText.setText(HR.names[position]);
-
-                //image.setImageResource(mImages[position]);
-            Button click = (Button) findViewById(R.id.btn_contact);
+            Button click = (Button) convertView.findViewById(R.id.btn_contact);
             click.setOnClickListener(new View.OnClickListener() {
 
                 @Override
@@ -139,7 +128,7 @@ public class HR extends ActionBarActivity {
             });
             ((ViewPager) container).addView(convertView, 0);
             return convertView;
-           // return imageView;
+
         }
 
         @Override
@@ -147,7 +136,6 @@ public class HR extends ActionBarActivity {
             ((ViewPager) container).removeView((RelativeLayout) object);
         }
     }
-
 
 
     @Override
