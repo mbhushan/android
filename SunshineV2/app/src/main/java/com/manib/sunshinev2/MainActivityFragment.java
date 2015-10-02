0ada5219f406fb678f1d72c7cecfee1c87a836b4
manibhushan <manibhushan.cs@gmail.com>
2015-10-02 17:47:37 +0530

    added list adapter

--- SunshineV2/app/src/main/java/com/manib/sunshinev2/MainActivityFragment.java
+++ SunshineV2/app/src/main/java/com/manib/sunshinev2/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +15,8 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -33,6 +36,9 @@ public class MainActivityFragment extends Fragment {
         };
 
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview, weekForecast);
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
